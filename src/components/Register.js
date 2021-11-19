f282ab8a074900270ee8e646bece7aab0dddc4b0
Dayana Panova <dzopino@abv.bg>
2021-11-20 00:55:42 +0200

    Setting the layout of the image and the form initially with Grid.Later i will refactor the style of the layou, for now is inline.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,18 +1,19 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useForm } from 'react-hook-form';
+import * as React from 'react'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import Link from '@mui/material/Link'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { registerSchema } from '../utils/validations';
+import { registerSchema } from '../utils/validations'
 import { useState } from 'react'
 import LogoComponent from '../assets/LogoComponent'
+import LandPage from '../assets/LandPage'
 
 const theme = createTheme({
 
@@ -55,64 +56,70 @@ export default function LogIn() {
 
     return (
         <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <LogoComponent />
-                    <Typography component="h1" variant="h5">
-                        WELCOME BACK!
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
-                        {FIELDS.map(({ name, componentProps }, index) => {
-                            const error = errors?.name?.message;
-                            const hasError = Boolean(error);
-                            return (
-                                <TextField
-                                    key={`${name}-${index}`}
-                                    {...componentProps}
-                                    {...register(name)}
-                                >
+            <CssBaseline />
+            <Grid container spacing={1}>
+                <Grid item md={4}>
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            padding: "40px"
+                        }}
+                    >
+                        <LogoComponent />
+                        <Typography component="h1" variant="h5">
+                            WELCOME BACK!
+                        </Typography>
+                        <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+                            {FIELDS.map(({ name, componentProps }, index) => {
+                                const error = errors?.name?.message;
+                                const hasError = Boolean(error);
+                                return (
+                                    <TextField
+                                        key={`${name}-${index}`}
+                                        {...componentProps}
+                                        {...register(name)}
+                                    >
 
-                                    {hasError && (
-                                        <TextField
-                                            helperText={error}
-                                        />
-                                    )}
-                                </TextField>
-                            )
-                        })}
+                                        {hasError && (
+                                            <TextField
+                                                helperText={error}
+                                            />
+                                        )}
+                                    </TextField>
+                                )
+                            })}
 
-                        <Link href="#" variant="body2">
-                            Forgot password
-                        </Link>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        //disabled={setDisabled}
-                        >
-                            LOG IN
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password
+                            </Link>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            //disabled={setDisabled}
+                            >
+                                LOG IN
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"You don't have an account? SIGN UP HERE"}
+                                    </Link>
+                                </Grid>
                             </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"You don't have an account? SIGN UP HERE"}
-                                </Link>
-                            </Grid>
-                        </Grid>
+                        </Box>
                     </Box>
-                </Box>
-            </Container>
+                </Grid>
+                <Grid item md={8}>
+                    <LandPage />
+                </Grid>
+            </Grid>
         </ThemeProvider>
     );
 }
